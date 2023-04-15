6b98b195254a6015e0849e86c31e76b51c8d427e
Jana <yanabling@gmail.com>
2023-04-15 14:50:35 +0700

    updated Character.java

--- src/test/java/ru/gloomyana/Character.java
+++ src/test/java/ru/gloomyana/Character.java
@@ -1,12 +1,10 @@
 package ru.gloomyana;
 
-
 public class Character {
     public String name;
     public String race;
 
     public classAndSpec classAndSpec;
-
     public static class classAndSpec {
         public String className;
         public String specName;
@@ -17,5 +15,4 @@ public class Character {
     public Integer killsCount;
     public Integer deathsCount;
     public Integer completedQuestsCount;
-
 }
